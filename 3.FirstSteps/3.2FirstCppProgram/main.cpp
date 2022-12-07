cc71a22a915c5a97a1454242353d09c3c3b1ead6
consultorsandro <consultorsandro@hotmail.com>
2022-12-07 20:17:17 -0300

    Seção 7 - aula 3.2

---
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -0,0 +1,12 @@
+// Esta aula trata da biblioteca de entrada iostream e estrutura básica de um programa C++
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    // Imprime o texto entre aspas e pula uma linha
+    cout << "Recomeço " << endl;
+    cout << "Outra linha " << endl;
+    return 0;
+}
