4c2b907f816da3bad32967b0af26798481c3c254
consultorsandro <consultorsandro@hotmail.com>
2022-12-27 15:13:33 -0300

    "Atualiza Gcc e Clang"

--- 4.VariablesAndDatatypes/4.2NumberSystems/main.cpp
+++ 4.VariablesAndDatatypes/4.2NumberSystems/main.cpp
@@ -1,19 +1,11 @@
-// Esta aula trata de números nas bases binária, decimal, hexadecimal e octal 
-
 #include <iostream>
 
+consteval int get_value(){
+    return 3;
+}
+
 int main(){
-   
-   //Declara  e inicializa variáveis. Na inicialização define a base
-   int number1 = 15; //decimal
-   int number2 = 017; //Octal
-   int number3 = 0x0F; //hexadecimal
-   int number4 = 0b00001111; //binary
-
-   std::cout << "number1 : " << number1 << std::endl;
-   std::cout << "number2 : " << number2 << std::endl;
-   std::cout << "number3 : " << number3 << std::endl;
-   std::cout << "number4 : " << number4 << std::endl;
-
-
+    constexpr int value = get_value();
+    std::cout << "value : " << value << std::endl;
+    return 0;
 }
